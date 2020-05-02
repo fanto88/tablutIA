ffcff905887df93f378e84bb6a201eb3a524292f
Alberto Mordonini <albmord@gmail.com>
2020-05-02 18:49:01 +0200

    parallel search

--- search/parallel_search.py
+++ search/parallel_search.py
@@ -1,20 +1,21 @@
 import importlib
-from multiprocessing import (Process, Manager, set_start_method)
+from multiprocessing import (Process, Manager)
 from search.search import (MinMaxAgent, possible_actions, resulting_state)
 
 import os
+import operator
 
 
 class ParallelMinMax(MinMaxAgent):
 
     def __init__(self, process_no, max_depth, max_time):
-        super().__init__(max_depth-1, max_time)
+        super(ParallelMinMax, self).__init__(max_depth-1, max_time)
         self.process_no = process_no
         self.jobs = []
 
         # Process-safe structures
         manager = Manager()
-        self.result = manager.list()
+        self._result = manager.list()
         self.checked = manager.dict()
 
     def make_decision(self, state, problem):
@@ -24,6 +25,8 @@ class ParallelMinMax(MinMaxAgent):
         first_level_states = list(map(lambda action: resulting_state(state, action, problem), list_actions))
         num_states = len(first_level_states)
 
+        # couples
+
         # Number of states assigned to each process
         cut = int(num_states/self.process_no) \
             if num_states > self.process_no \
@@ -31,7 +34,7 @@ class ParallelMinMax(MinMaxAgent):
         cut_first_level_states = list(chunks(first_level_states, cut))
 
         # Preparing workers
-        self.jobs = [Process(target=self._worker, args=(states, problem, self.result))
+        self.jobs = [Process(target=self._worker, args=(states, problem, self._result))
                             for states in cut_first_level_states]
 
         # Start workers
@@ -40,15 +43,23 @@ class ParallelMinMax(MinMaxAgent):
         # Wait for workers
         [p.join() for p in self.jobs]
 
-        # TODO: Analizza risultati di self.result e restituisci l'azione più vantaggiosa
-        return first_level_states
+        self._result.sort(key=operator.itemgetter(1))
+        best_action = list(filter(lambda x: x[0] == self._result[0][0],
+                                  list(zip(first_level_states, list_actions))))[0][1]
+        return best_action
 
     def _worker(self, states, problem, out):
-        self.max_depth -= 1  # Il primo livello è già stato espanso
         best_actions = [self.choose_action(state, problem) for state in states]
-        out.append(best_actions)
-        print("<PID {}> Stati: {}, Risultati: {} Stati saltati: {}".format(
-            os.getpid(), states, best_actions, self.node_skipped))
+
+        # Visto che il primo livello è stato diviso
+        self.max_depth += 1
+
+        values = [a[1] for a in best_actions]
+        partial_result = list(zip(states, values))
+        out += partial_result
+        print("<PID {}> Stati: {}, Risultati (stato, valore): {} Stati saltati: {}".format(
+            os.getpid(), states, partial_result, self.node_skipped))
+
 
 
 # Util
