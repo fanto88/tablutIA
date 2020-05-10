1ebf08ced3d509b1f2b289d99d8317a73002def5
Alberto Mordonini <albmord@gmail.com>
2020-05-10 12:26:37 +0200

    skip duplicate states

--- tablut/search/search.py
+++ tablut/search/search.py
@@ -31,22 +31,20 @@ class MinMaxAgent:
     def _already_checked_result(self, state):
         return self.checked[state]
 
-    def _mark_checked(self, state, values):
-        self.checked[state] = values    # values = (value of single state, action)
+    def _mark_checked(self, state):
+        self.checked[state] = 1    # values = (value of single state, action)
 
     # TODO: si può provare a ottimizzare l'algoritmo e non restituire per ogni stato (valore, azione) ma solo valore /
         # alla fine (valore, azione) è contenuto nel dict checked
     # TODO: se fai quanto detto sopra, potresti togliere i nodi
-    def _minimax(self, node, problem, max_turn, alpha, beta):
-        print("<PID {}>: stato: {}".format(os.getpid(), node.state))
+    def _minimax(self, node, problem, maximize, alpha, beta):
 
         # Controlla se lo stato corrente è già stato elaborato
-        """if self._already_checked(node.state):
-            print(os.getpid(),"Stato", node.state," già elaborato")
+        if self._already_checked(node.state):
             self.node_skipped += 1
-            return (float('-inf'), None) if max_turn else (float('inf'), None)
+            return (float('-inf'), None) if maximize else (float('inf'), None)
         else:
-            self._mark_checked(node.state, (None, None))"""
+            self._mark_checked(node.state)
 
         # Ricerca termina se:
         #   -E' uno stato terminale
@@ -59,33 +57,32 @@ class MinMaxAgent:
             values = self.utility(node.state, problem), node.action
 
             # TODO: risolvi problema del mark_checked
-            #self._mark_checked(node.state, values)
+            self._mark_checked(node.state)
             return values
 
         self.node_expanded += 1
-        value = float('-inf') if max_turn else float('inf')
+        value = float('-inf') if maximize else float('inf')
         best_action = ''
 
         list_actions = self.possible_actions(node.state, problem)
         for action in list_actions:  # TODO: vettorizza il codice
             new_state = self.resulting_state(node.state, action, problem)
             new_node = Node(new_state, node, action, node.path_cost+1)
-            child_value, child_action = self._minimax(new_node, problem, not max_turn, alpha, beta)
-            if max_turn and value < child_value:
+            child_value, child_action = self._minimax(new_node, problem, not maximize, alpha, beta)
+            if maximize and value < child_value:
                 value = child_value
                 best_action = action
                 alpha = max(alpha, value)
                 if beta <= alpha:
                     break
 
-            elif (not max_turn) and value > child_value:
+            elif (not maximize) and value > child_value:
                 value = child_value
                 best_action = action
                 beta = min(beta, value)
                 if beta <= alpha:
                     break
 
-        #self._mark_checked(node.state, value)
         return value, best_action
 
     # Utils
