62a182f2ab595b8e50875fa683243cf3e3870571
davide <fantinidavide9@gmail.com>
2020-05-10 07:37:34 -0500

    Code Refactor and small bug fix

--- tablut/client/tablut_client.py
+++ tablut/client/tablut_client.py
@@ -11,25 +11,25 @@ class Client(ConnectionHandler):
 
     def __init__(self, port, host, role, timeout):
         super().__init__(port, host)
-        self.__player_name = config.PLAYER_NAME
-        self.__role = role
-        self.__timeout = timeout
-        self.__state = TablutState(self.__role)
+        self.player_name = config.PLAYER_NAME
+        self.color = role
+        self.timeout = timeout
+        self.state = TablutState(self.color)
 
     def run(self):
         """Implements the logic of the client."""
         try:
             self.connect()  # Connecting to the server
-            self.send_string(self.__player_name)  # Sending the name
-            self.__state.load_state_from_json(self.read_string())  # Read the initial state
+            self.send_string(self.player_name)  # Sending the name
+            self.state.load_state_from_json(self.read_string())  # Read the initial state
             while True:  # Game loop
-                if self.__role == self.__state.turn:  # check if our turn or not
-                    search = parallel_search.ParallelMinMax(2, 3, self.__timeout)
-                    action = search.make_decision(self.__state, TablutProblem())
+                if self.color == self.state.turn:  # check if our turn or not
+                    search = parallel_search.ParallelMinMax(2, 3, self.timeout - 5)
+                    action = search.make_decision(self.state, TablutProblem())
                     if action is not None:
-                        self.__state.move(action)  # Execute the move
-                        self.send_string(action.to_server_format())  # send the move to the server
-                self.__state.load_state_from_json(self.read_string())  # read the new state
+                        self.state.move(action)  # Execute the action
+                        self.send_string(action.to_server_format())  # send the action to the server
+                self.state.load_state_from_json(self.read_string())  # read the new state
         except ConnectionResetError:
             pass
         finally:
